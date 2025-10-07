064718089556341b187c012efd02973675a3e06e
FilippoCasarosa <filippo.casarosa1@gmail.com>
2025-10-07 11:55:50 +0200

    coverage test

--- server/src/test/java/it/skinjobs/api/CredentialApiTest.java
+++ server/src/test/java/it/skinjobs/api/CredentialApiTest.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -55,10 +55,10 @@ public class CredentialApiTest {
     @Autowired
     private CredentialsProperties properties;
 
-    @MockBean
+    @SpyBean
     private Credentials credentials;
 
-    @MockBean
+    @SpyBean
     private Sessions sessions;
 
     @Autowired
